dffb282353740f6b7d1ec21f2fdebfd7fe551541
MichaelDHuy <dachuy87@email.com>
2022-10-04 06:34:06 +0000

    Change codes with app.set

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -24,7 +26,7 @@ app.get("/set", (req, res) => {
  app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
  });
- 
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
