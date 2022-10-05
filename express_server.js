54ece039a263ff169df5d2b9bd6f043c6c9d2dae
MichaelDHuy <dachuy87@email.com>
2022-10-05 22:55:09 +0000

    Added login logout functions to new git branchfeatu feature/cookies

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const cookieParser = require('cookie-parser')
 const PORT = 8080; // default port 8080
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.set("view engine", "ejs");
 
 const generateRandomString = function() {
@@ -25,31 +27,6 @@ app.post("/urls", (req, res) => {
   urlDatabase[shortURL] = req.body.longURL;
   res.redirect(`/urls/${shortURL}`)
 });
-app.get("/u/:id", (req, res) => {
-  // const longURL = ...
-  const longURL = urlDatabase[req.params.id];
-  res.redirect(longURL);
-});
-
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[`${req.params.id}`]  };
-  res.render("urls_show", templateVars);
-});
-
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-app.get("/hello", (req, res) => {
-  const templateVars = { greeting: "Hello World!" };
-  res.render("hello_world", templateVars);
-});
 
 app.post("/urls/:id/delete", (req, res) => {
   const id = req.params.id;
@@ -62,6 +39,57 @@ app.post("/urls/:id/update", (req, res) => {
   urlDatabase[id] = req.body.update;
   res.redirect(`/urls/${id}`);
 });
+
+app.post("/urls/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie("username", username);
+  res.redirect("/urls");
+})
+
+app.post("/urls/logout", (req, res) =>{
+  res.clearCookie("username");
+  res.redirect("/urls");
+})
+
+app.get("/u/:id", (req, res) => {
+  // const longURL = ...
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  const templateVars = { 
+    username: req.cookies["username"]
+  }; 
+  res.render("urls_new", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { 
+    id: req.params.id, 
+    longURL: urlDatabase[`${req.params.id}`],
+    username: req.cookies["username"] 
+  };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/", (req, res) => {
+  res.redirect("/urls");
+});
+
+app.get("/hello", (req, res) => {
+  const templateVars = { greeting: "Hello World!" };
+  res.render("hello_world", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
