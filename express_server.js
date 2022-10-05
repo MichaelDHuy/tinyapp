2462302fb15a1c0ba145c474cbcd3ca020fd9c55
MichaelDHuy <dachuy87@email.com>
2022-10-05 00:45:48 +0000

    Finished shorten url 2 session

--- express_server.js
+++ express_server.js
@@ -1,39 +1,37 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
 const generateRandomString = function() {
   const char = "QWERTYUIOPLKJHGFDSAZXCVBNMqwertyuioplkjhgfdsazxcvbnm1234567890";
   let randomString = "";
   for (let i = 0; i < 6; i++) {
     const randomChar = Math.floor(Math.random() * char.length);
-    randomString += randomChar;
+    randomString += char[randomChar];
   }
   return randomString;
 };
-
-app.set("view engine", "ejs");
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.post("/urls", (req, res) => {
+  let shortURL = generateRandomString();
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`)
 });
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/u/:id", (req, res) => {
+  // const longURL = ...
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
 });
-app.get("/hello", (req, res) => {
-  const templateVars = { greeting: "Hello World!" };
-  res.render("hello_world", templateVars);
-});
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
- });
- app.use(express.urlencoded({ extended: true }));
- app.get("/urls", (req, res) => {
+
+app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
@@ -41,13 +39,18 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[`${req.params.id}`]  };
   res.render("urls_show", templateVars);
 });
-app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
 });
+app.get("/hello", (req, res) => {
+  const templateVars = { greeting: "Hello World!" };
+  res.render("hello_world", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
